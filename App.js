ddd57519c0a6e5b43e02ed4ecdca68037f2d08fd
Arfin Chowdhury Arif <arfinchowdhuryarif7@gmail.com>
2021-05-06 17:14:33 +0600

    my first react native app

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './assets/components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.taskswappers}>
+          <Text style={styles.sectionTitle}> Today's Task</Text>
+      </View>
+      <View style={styles.items}>
+        <Task text={'text1'}/>
+        <Task text={'text1'}/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+      container: {
+        flex: 1,
+        backgroundColor: '#E8EAED',
+      },
+      taskswappers:{
+        paddingTop:80,
+        paddingHorizontal:20,
+      },
+      sectionTitle:{
+        fontSize:24,
+        font:'bold'
+      },
+      items:{
+        marginTop:30,
+      }
 });
---
+++ assets/components/Maps.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Maps() {
+    return (
+        <View>
+            <Text>This is maps components</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
